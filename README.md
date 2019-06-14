2ac46931636b3da949b820b0ca96132695b1c041
linwenjun <linwenjun@163.com>
2019-06-14 03:20:00 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Unit Test Basic 
+
+## Business Requirement
+
+This is a part of implementation code pick up from a Internal reimbursement system. you should write unit test for it to make sure it can work in production.
+
+
+## Practice Requirement:
+- ** Write test cases after clearly understanding the implemented code**
+- ** Complete tests in the existing test file and ensure that the test coverage of the test category of this test file is 100%**
+- Unit testing should cover all core business logic
+- Make unit test refactoring with small steps
+- ** Submit code in small steps with a comment on meaning **
+- Complete unit tests on the corresponding file under the **src/test** catalog of project
+
+#### Environment Requirement
+- Java 8
+- Intellj IDEA
+- JUnit 5
+- AssertJ 3.10.0
+- Gradle
+
+#### How to Start
+
+- You can fork the following github repository as starting point  
+- Please submit your own github repository address in the answer field.  
+
+#### How to Test
+- Implement `./gradlew clean test` under root catalogue and view test result
+ 
+## Practice Output && Submit
+- You should implement the test code and push your repo to github repo named **unit-test-basic**.
+- Commit your repostory link to complete this Question.
+
---
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -0,0 +1,47 @@
+package ExpenseService;
+
+import ExpenseService.Exception.UnexpectedProjectTypeException;
+import ExpenseService.Expense.ExpenseType;
+import ExpenseService.Project.Project;
+import ExpenseService.Project.ProjectType;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+class ExpenseServiceTest {
+    @Test
+    void should_return_internal_expense_type_if_project_is_internal() throws UnexpectedProjectTypeException {
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void should_return_expense_type_A_if_project_is_external_and_name_is_project_A() throws UnexpectedProjectTypeException {
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void should_return_expense_type_B_if_project_is_external_and_name_is_project_B() throws UnexpectedProjectTypeException {
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void should_return_other_expense_type_if_project_is_external_and_has_other_name() throws UnexpectedProjectTypeException {
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void should_throw_unexpected_project_exception_if_project_is_invalid() {
+        // given
+        // when
+        // then
+    }
+}
