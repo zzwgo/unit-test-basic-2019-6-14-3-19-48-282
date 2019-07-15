16f5ebe30307335efbe76d59989c8aa7f629119d
zzw <kenzzw@163.com>
2019-07-15 21:20:37 +0800

    Test: test should_return_internal_expense_type_if_project_is_internal()

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -5,16 +5,17 @@ import ExpenseService.Expense.ExpenseType;
 import ExpenseService.Project.Project;
 import ExpenseService.Project.ProjectType;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExpenseServiceTest {
     @Test
     void should_return_internal_expense_type_if_project_is_internal() throws UnexpectedProjectTypeException {
         // given
+        Project project = new Project(ProjectType.INTERNAL,"xxx");
         // when
+        ExpenseType result = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
         // then
+        assertEquals(ExpenseType.INTERNAL_PROJECT_EXPENSE,result);
     }
 
     @Test
